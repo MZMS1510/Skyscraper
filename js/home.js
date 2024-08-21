fb251db25d1e31b04700c8096449fca6c07c3cf8
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-08-21 02:24:10 -0300

    ADD: New files - Home Page

---
+++ js/home.js
@@ -0,0 +1,49 @@
+document.getElementById("open-menu").addEventListener("click", function(){
+    let menu = document.getElementById("menu")
+
+    if (menu.style.left = "-460px"){
+        menu.style.left = "0"
+        menu.style.transition = "1s ease all"
+    }
+})
+
+document.getElementById("close-menu").addEventListener("click", function(){
+    let menu = document.getElementById("menu")
+
+    if(menu.style.left = "0"){
+        menu.style.left = "-460px"
+        menu.style.transition = "1s ease all"
+    }
+})
+
+document.getElementById("open-universe").addEventListener("click", function(){
+    let menu_universe = document.getElementById("menu-universe")
+    let menu = document.getElementById("menu")
+    let close_universe = document.getElementById("close-universe")
+
+    if (menu_universe.style.left = "-460px"){
+        menu.style.left = "-460px"
+        menu.style.transition = "none"
+        close_universe.style.fontWeight = "bold"
+        close_universe.style.border = "2px solid #5a5b5b"
+        close_universe.style.textDecoration = "none"
+        close_universe.style.color = "white"
+        menu_universe.style.left = "0"
+        menu_universe.style.transition = "1s ease all"
+    }
+})
+
+document.getElementById("close-universe").addEventListener("click", function(){
+    let menu_universe = document.getElementById("menu-universe")
+    let menu = document.getElementById("menu")
+    let close_universe = document.getElementById("close-universe")
+
+    if(menu_universe.style.left = "0"){
+        menu.style.left = "0"
+        menu.style.transition = "none"
+        menu_universe.style.left = "-460px"
+        close_universe.style.border = "2px solid black"
+        close_universe.style.color = "black"
+        menu_universe.style.transition = "1s ease all"
+    }
+})
